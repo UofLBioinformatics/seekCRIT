25d75d5522d2a7786e0a5c011f3222346cc2c98a
UofLBioinformatics <uoflbioinformatics@gmail.com>
2017-10-05 14:28:50 -0400

    Update testrun.sh

--- testrun.sh
+++ testrun.sh
@@ -5,9 +5,6 @@ if [ "$#" -ne 2 ]; then
     exit 1
 fi
 
-
-
-
 if ! [ -x "$(command -v STAR)" ]; then
   echo ' STAR is not installed.' >&2
   echo 'install STAR first from https://github.com/alexdobin/STAR '
@@ -16,14 +13,12 @@ fi
 
 cd seekCRIT
 mkdir genomeindex
-
 echo "Building genome index for star... "
-
 STAR --runThreadN 4 --runMode genomeGenerate --genomeDir genomeindex/ --genomeFastaFiles $2 --sjdbGTFfile $1 --sjdbOverhang 100
 
+
 echo "Starting testing seekCRIT... "
-
-
+mkdir SEtest
 python3 seekCRIT.py -o SEtest -t SE --aligner STAR -fa $2 -ref ref/rn6.ref.txt --genomeIndex genomeindex -s1 testData/CRTL12.fastq -s2 testData/IR12.fastq -gtf $1 --threadNumber 12
 echo "seekCRIT tested with success "
 
