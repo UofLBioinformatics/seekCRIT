9a9c3dbb40b3b01ca60ffff8b4ca3ec2477b7432
UofLBioinformatics <uoflbioinformatics@gmail.com>
2017-10-05 12:40:59 -0400

    Add files via upload

---
+++ testrun.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ "$#" -ne 2 ]; then
+    echo "seekCRIT Setup Tool usage: testrun.sh /path/to/gtf.file /path/to/genome.fasta.file  "
+    exit 1
+fi
+
+
+
+
+if ! [ -x "$(command -v STAR)" ]; then
+  echo ' STAR is not installed.' >&2
+  echo 'install STAR first from https://github.com/alexdobin/STAR '
+  exit 1
+fi
+
+cd seekCRIT
+mkdir genomeindex
+
+echo "Building genome index for star... "
+
+STAR --runThreadN 4 --runMode genomeGenerate --genomeDir genomeindex/ --genomeFastaFiles $2 --sjdbGTFfile $1 --sjdbOverhang 100
+
+echo "Starting testing seekCRIT... "
+
+
+python3 seekCRIT.py -o SEtest -t SE --aligner STAR -fa $2 -ref ref/rn6.ref.txt --genomeIndex genomeindex -s1 CTRL.fastq -s2 IR12.fastq -gtf $1 --threadNumber 12
+
+
